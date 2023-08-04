c8f92ea4c16493915aed1b84d5aefea28222cd18
Richard Jiang <jiangr@sbschools.net>
2023-08-03 21:39:55 -0400

    sorted the files into finished programs and unfinished programs. I also finished the digit histogram program and copied the longestline program from the book

--- digithistogram.c
+++ digithistogram.c
@@ -6,7 +6,6 @@ main()
 {
 	int c, i, tmp, nwhite, nother, maxn;
 	int ndigit[10];
-	char maxd;	//ascii code of the digit that has the most
 
 	nwhite = nother = 0;
 	for (i = 0; i <= 9; ++i)	//makes every element of the array start at 0
@@ -21,18 +20,11 @@ main()
 			++nother;
 	
 	maxn = nwhite;	// there is the digit that occurs the most number of times, and maxn is the number of times that digit occurs
-	maxd = 1;
-	if (maxn < nother)	{
+	if (maxn < nother)
 		maxn = nother;
-		maxd = 2;
-	}
 	for (i = 0; i <= 9; ++i)
-		if (maxn < ndigit[i])	{
+		if (maxn < ndigit[i])
 			maxn = ndigit[i];
-			maxd = i + '0';	// for example, if i=2 (when it is looking at how many times the number 2 occurs), then i+'0' is the ascii code for two
-		}
-	// printf("DEBUG: maxn=%d\n", maxn);
-	// printf("DEBUG: maxd=%c\n", maxd);
 	
 	nwhite = maxn - nwhite;
 	nother = maxn - nother;
@@ -48,8 +40,18 @@ main()
 				printf("  ==");
 			--ndigit[tmp];
 		}
+		if (nother > 0)
+			printf("     ");
+		else if (nother <= 0)
+			printf("   ==");
+		--nother;
+		if (nwhite > 0)
+			printf("     ");
+		else if (nwhite <= 0)
+			printf("      ==");
+		--nwhite;
 		printf("\n");
 	}
-	printf("________________________________________\n");
-	printf("  0   1   2   3   4   5   6   7   8   9 \n\n");
+	printf("_____________________________________________________________\n");
+	printf("  0   1   2   3   4   5   6   7   8   9   other   white space\n\n");
 }
